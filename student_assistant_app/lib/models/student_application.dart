0bce3ebcea92b8122e08c1e8d3e0feadddf8aaa3
Musa Bonga <musabonga253@gmail.com>
2026-05-10 03:38:40 +0200

    Removed code in student_application.dart – Student No: 224022456

--- student_assistant_app/lib/models/student_application.dart
+++ student_assistant_app/lib/models/student_application.dart
@@ -1,17 +1,1 @@
-class StudentApplication {
-  final int id;
-  final String studentName;
-  final String yearOfStudy;
-  final String module1;
-  final String? module2;
-  String status; // Pending, Approved, Rejected
 
-  StudentApplication({
-    required this.id,
-    required this.studentName,
-    required this.yearOfStudy,
-    required this.module1,
-    this.module2,
-    this.status = "Pending",
-  });
-}
