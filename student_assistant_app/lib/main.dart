69d68712eb462405756eb5a2253bce48881be586
Musa Bonga <musabonga253@gmail.com>
2026-05-10 02:46:42 +0200

    Added empty screen stubs – Student No: 224022456

--- student_assistant_app/lib/main.dart
+++ student_assistant_app/lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../viewmodels/application_viewmodel.dart';
+import '../route_manager.dart';
 
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ApplicationViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Student Assistant App',
+      initialRoute: RouteManager.login,
+      onGenerateRoute: RouteManager.generateRoute,
+    );
+  }
+}
